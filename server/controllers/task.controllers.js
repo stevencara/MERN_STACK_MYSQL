87f1f12ca6710bae1d610d7ce90ad64345673d72
Steven <stevencara2009@hotmail.com>
2024-11-27 19:56:58 -0500

    se realiza modulo para peticiones PUT de una task Endpoint

--- server/controllers/task.controllers.js
+++ server/controllers/task.controllers.js
@@ -1,5 +1,6 @@
 import { pool } from "../db.js";
 
+/* -------------------------- VER TODAS LAS TAREAS -------------------------- */
 export const getTasks = async (req, res) => {
   const [result] = await pool.query(
     "SELECT * FROM tasks ORDER BY createAt ASC"
@@ -7,6 +8,7 @@ export const getTasks = async (req, res) => {
   res.json(result);
 };
 
+/* -------------------------- VISUALIZAR UNA TAREA -------------------------- */
 export const getTask = async (req, res) => {
   const [result] = await pool.query("SELECT * FROM tasks WHERE id = ?", [
     req.params.id,
@@ -19,6 +21,7 @@ export const getTask = async (req, res) => {
   res.send(result[0]);
 };
 
+/* ----------------------------- CREAR UNA TAREA ---------------------------- */
 export const createTask = async (req, res) => {
   const { title, description } = req.body;
   const [result] = await pool.query(
@@ -33,10 +36,19 @@ export const createTask = async (req, res) => {
   });
 };
 
-export const updateTask = (req, res) => {
-  res.send("actualizando tarea");
+/* -------------------------- ACTUALIZAR UNA TAREA -------------------------- */
+export const updateTask = async (req, res) => {
+  const { title, description } = req.body;
+
+  const result = await pool.query("UPDATE tasks SET ? WHERE id = ?", [
+    req.body,
+    req.params.id,
+  ]);
+
+  res.json(result);
 };
 
+/* ----------------------------- ELIMINAR TAREA ----------------------------- */
 export const deleteTask = async (req, res) => {
   const [result] = await pool.query("DELETE FROM tasks WHERE id = ?", [
     req.params.id,
@@ -44,5 +56,5 @@ export const deleteTask = async (req, res) => {
 
   if (result.affectedRows === 0)
     return res.status(404).json({ message: "task not found" });
-    return res.sendStatus(204); 
+  return res.sendStatus(204);
 };
