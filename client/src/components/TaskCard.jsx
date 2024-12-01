7f05ef7d1ad2e0786795b526c3c6901c5a907e83
Steven <stevencara2009@hotmail.com>
2024-12-01 15:56:50 -0500

    Delete tasks Frontend

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { deleteTaskRequest } from "../api/tasks.api";
 
-function TaskCard( {task}) {
+function TaskCard({ task }) {
+  const handleDelete = async (id) => {
+    try {
+      const response = await deleteTaskRequest(id);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h2>{task.title}</h2>
       <p>{task.description}</p>
       <span>{task.done == 1 ? "✅" : "❌"}</span>
       <span>{task.createAt}</span>
-      <button>Delete</button>
+      <button onClick={() => handleDelete(task.id)}>Delete</button>
       <button>Edit</button>
     </div>
   );
--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { getTaskRequest } from "../api/tasks.api";
-import  TaskCard  from "../components/TaskCard"
+import TaskCard from "../components/TaskCard";
 
 function TasksPage() {
   const [tasks, setTasks] = useState([]);
@@ -14,16 +14,16 @@ function TasksPage() {
     loadTasks();
   }, []);
 
+  function renderMain() {
+    
+    if (tasks.length === 0) return <h1>No tasks yet</h1>
+    return tasks.map((task) => <TaskCard task={task} key={task.id} />);
+  }
+
   return (
     <div>
       <h1>Tasks</h1>
-
-      {
-        tasks.map((task) => (
-          <TaskCard task={task} key={task.id} />
-      ))
-      }
-
+      {renderMain()}
     </div>
   );
 }
