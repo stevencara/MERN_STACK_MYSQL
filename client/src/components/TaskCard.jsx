752a4fc9ed84e46a555a6ce9ed7bf9ca3e199299
Steven <stevencara2009@hotmail.com>
2024-12-02 20:27:15 -0500

    toggle done frontend

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -3,8 +3,12 @@ import { useTasks } from "../context/TaskProvider";
 import { useNavigate } from "react-router-dom";
 
 function TaskCard({ task }) {
-  const { deleteTask } = useTasks();
+  const { deleteTask, toggleTaskDone } = useTasks();
   const navigate = useNavigate();
+
+  const handleDone = async () => {
+    await toggleTaskDone(task.id)
+  }
 
   return (
     <div>
@@ -14,6 +18,7 @@ function TaskCard({ task }) {
       <span>{task.createAt}</span>
       <button onClick={() => deleteTask(task.id)}>Delete</button>
       <button onClick={() => navigate(`/edit/${task.id}`)}>Edit</button>
+      <button onClick={() => handleDone(task.done)}>Toggle Task</button>
     </div>
   );
 }
