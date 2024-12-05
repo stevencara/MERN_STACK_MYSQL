8638faccaddf822f16d312c65864b29501e8b9d3
Steven <stevencara2009@hotmail.com>
2024-12-05 16:38:01 -0500

    configuracion de constantes en BD para luego hacer deploy

--- server/db.js
+++ server/db.js
@@ -1,11 +1,10 @@
-import { createPool } from 'mysql2/promise';
+import { createPool } from "mysql2/promise";
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME } from "./config.js";
 
 export const pool = new createPool({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Operacioness3',
-    database: 'tasksdb',
+  host: DB_HOST,
+  port: DB_PORT,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
 });
-
-
