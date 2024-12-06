c15add5b04fb9a61b1bd61fa35ac75b824e1a8e2
Steven <stevencara2009@hotmail.com>
2024-12-06 17:25:01 -0500

    prueba 5

--- server/config.js
+++ server/config.js
@@ -1,7 +1,8 @@
 export const PORT = process.env.PORT || 4000;
 
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "Operacioness3";
-export const DB_NAME = process.env.DB_NAME || "tasksdb";
-export const DB_PORT = process.env.DB_PORT || "3306";
+export const MYSQL_ADDON_HOST = process.env.MYSQL_ADDON_HOST || "localhost";
+export const MYSQL_ADDON_USER = process.env.MYSQL_ADDON_USER || "root";
+export const MYSQL_ADDON_PASSWORD = process.env.MYSQL_ADDON_PASSWORD || "Operacioness3";
+export const MYSQL_ADDON_DB = process.env.MYSQL_ADDON_DB || "tasksdb";
+export const MYSQL_ADDON_PORT = process.env.MYSQL_ADDON_PORT || 3306;
+export const MYSQL_ADDON_URI = process.env.MYSQL_ADDON_URI || "mysql://u5y0wpk6dqchq9er:aIaMTtnF332OTI8WrzHd@bsbx5epk3lrgrzhy9fix-mysql.services.clever-cloud.com:3306/bsbx5epk3lrgrzhy9fix"
--- server/db.js
+++ server/db.js
@@ -1,10 +1,18 @@
 import { createPool } from "mysql2/promise";
-import { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME } from "./config.js";
+import {
+  MYSQL_ADDON_HOST,
+  MYSQL_ADDON_USER,
+  MYSQL_ADDON_PASSWORD,
+  MYSQL_ADDON_DB,
+  MYSQL_ADDON_PORT,
+  MYSQL_ADDON_URI
+} from "./config.js";
 
 export const pool = new createPool({
-  host: DB_HOST,
-  port: DB_PORT,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
+  host: MYSQL_ADDON_HOST,
+  port: MYSQL_ADDON_PORT,
+  user: MYSQL_ADDON_USER,
+  password: MYSQL_ADDON_PASSWORD,
+  database: MYSQL_ADDON_DB,
+  uri: MYSQL_ADDON_URI,
 });
