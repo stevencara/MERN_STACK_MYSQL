bd114580d241a116e4bb0a5ff76b406b6fc2d586
Steven <stevencara2009@hotmail.com>
2024-12-06 00:29:18 -0500

    prueba despliegue

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,21 +1,22 @@
 import axios from "axios";
 
-export const getTasksRequest = async () =>
-  await axios.get("http://localhost:4000/tasks");
+const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
+
+export const getTasksRequest = async () => await axios.get(`${URL}/tasks`);
 
 export const createTaskRequest = async (task) =>
-  await axios.post("http://localhost:4000/tasks", task);
+  await axios.post(`${URL}/tasks`, task);
 
 export const deleteTaskRequest = async (id) =>
-  await axios.delete(`http://localhost:4000/tasks/${id}`);
+  await axios.delete(`${URL}/tasks/${id}`);
 
 export const getTaskRequest = async (id) =>
-  await axios.get(`http://localhost:4000/tasks/${id}`);
+  await axios.get(`${URL}/tasks/${id}`);
 
 export const updateTaskRequest = async (id, newFields) =>
-  await axios.put(`http://localhost:4000/tasks/${id}`, newFields);
+  await axios.put(`${URL}/tasks/${id}`, newFields);
 
 export const toggleTaskDoneRequest = async (id, done) =>
-  await axios.put(`http://localhost:4000/tasks/${id}`, {
+  await axios.put(`${URL}/tasks/${id}`, {
     done,
   });
